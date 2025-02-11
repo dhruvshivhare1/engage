d1e0f1b18386702ebd7d476ce2a6ef14a52290fe
dhruvshivhare1 <dhruvshivhare16@gmail.com>
2025-02-12 01:29:38 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/engage/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
